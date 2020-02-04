b754dd38342b10d12b4d97d1267f9317cfecf0b2
Kevin Clark <kevin.clark93@gmail.com>
2020-02-04 21:34:28 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,10 +12,10 @@ def print_first_directors_movie_titles
   spielberg_movies_index = 0 
   #while the movie index is less than the length of the list of movies made by spielberg..
   while spielberg_movies_index < directors_database[0][:movies].length do
-      
+    
     spielberg_movies_index ++
   end
 end
 
 #Zero is the director, one is a movie theyve made
-pp directors_database[0][:movies].length
+#pp directors_database[0][:movies].length
